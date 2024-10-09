98dc1671244cd2735470a9998928185777acd0c5
Bernard Antwi Adabankah <adabankah.com@gmail.com>
2024-10-10 00:14:24 +0100

    Update eks-install.sh

--- eks-install.sh
+++ eks-install.sh
@@ -2,14 +2,14 @@
 
 # Creation of EKS cluster
 eksctl create cluster \
-  --name ghaCluster \
+  --name eks-demo \
   --region eu-west-2 \
   --nodegroup-name standard-workers \
   --node-type t3.medium \
   --nodes 3 \
   --nodes-min 1 \
   --nodes-max 4 \
-  --ssh-public-key $KEY_NAME \
+  --ssh-public-key April2024key1 \
   --managed
 
 #to destroy issue this command:
